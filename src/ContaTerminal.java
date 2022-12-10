a644e12dd017585c3056598b585e74e685ad1c94
Ivanildo Pontual Neto <ivanildopontual95@gmail.com>
2022-12-09 22:12:27 -0400

    Segundo Commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -6,16 +6,14 @@ public class ContaTerminal {
 
 		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
 
-		System.out.println("Por favor, informe seu primeiro nome:");
-		String nomeCliente = sc.next();
-		System.out.println("Por favor, informe seu sobrenome:");
-		String sobrenomeCliente = sc.next();
+		System.out.println("Por favor, informe seu nome completo:");
+		String nomeCliente = sc.nextLine();
 		System.out.println("Por favor, informe o número da agência de sua preferência:");
 		String agencia = sc.next();
 		System.out.println("Por favor, crie um número da conta:");
 		int conta = sc.nextInt();
 		System.out.println("Por favor, realize um depósito para possuir um valor inicial:");
 		double saldo = sc.nextDouble();
-		System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente + ", obrigado por criar sua conta no PontualBank. Sua agência é a de número " + agencia + " e sua conta é a de número " + conta + ", com saldo inicial de R$" + saldo + ".");
+		System.out.println("Olá " + nomeCliente + ", obrigado por criar sua conta no PontualBank. Sua agência é a de número " + agencia + " e sua conta é a de número " + conta + ", com saldo inicial de R$" + saldo + ".");
 	}
 }
